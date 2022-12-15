94b258f4bfdc1dc7ed6ec83c7f5679d709118849
bradenfarr <74750016+bradenfarr@users.noreply.github.com>
2022-12-15 10:36:11 -0700

    updated result styles/animation

--- script.js
+++ script.js
@@ -3,19 +3,22 @@ const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
 const resultText = document.querySelector(".result");
 
+let cooldown = false;
+const RECHARGE_TIME = 2000;
+
+// computerPlay function returns the computers choice
+
 function computerPlay () {
     let values = ["rock", "paper", "scissors"];
     let computerChoice = values[Math.floor(Math.random() * values.length)];
     return(computerChoice);
 } 
 
+// playRound function defines the logic of the game
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-            let x = resultText
-            x.style.marginRight = "0px"
-            x.style.marginLeft = "0px"
-            x.style.transition = "1s"
-            return resultText.textContent = "This round is a tie!";
+        return tie()
     };
     if (playerSelection === "rock") {
         if (computerSelection === "paper") {
@@ -40,51 +43,72 @@ function playRound(playerSelection, computerSelection) {
     };
 };
 
-// loss(playerSelection, computerSelection);
-// win(playerSelection, computerSelection);
+// Win Loss and Tie functions to alter resultText based on the results
 
 function loss(playerChoice, computerChoice) {
-    let result = `Sorry you lost, ${computerChoice} beats ${playerChoice}.`
-    let x = resultText
-    x.style.marginRight = "0px"
-    x.style.marginLeft = "300px"
-    x.style.transition = "1s"
-    resultText.textContent = result
+    resultText.removeAttribute("id", "lossAnimation");
+    void resultText.offsetWidth;
+    let x = resultText;
+    x.style.marginLeft = "300px";
+    x.style.marginRight = "0px";
+    x.style.transition = "1s";
+    resultText.setAttribute("id", "lossAnimation");
+    let result = `Sorry you lost, ${computerChoice} beats ${playerChoice}.`;
+    resultText.textContent = result;
 }
 
 function win(playerChoice, computerChoice) {
-    let x = resultText
-    x.style.marginLeft= "0px"
-    x.style.marginRight = "400px"
-    x.style.transition = "1s"
-    let result = `You won! ${playerChoice} beats ${computerChoice}.`
-    resultText.textContent = result
+    resultText.removeAttribute("id", "winAnimation");
+    void resultText.offsetWidth;
+    let x = resultText;
+    x.style.marginLeft = "0px";
+    x.style.marginRight = "400px";
+    x.style.transition = "1s";
+    resultText.setAttribute("id", "winAnimation");
+    let result = `You won, ${playerChoice} beats ${computerChoice}!`;
+    resultText.textContent = result;
 }
 
+function tie() {
+    resultText.removeAttribute("id", "tieAnimation");
+    void resultText.offsetWidth;
+    let x = resultText;
+    x.style.marginLeft = "0px";
+    x.style.marginRight = "0px";
+    x.style.transition = "1s";
+    resultText.setAttribute("id", "tieAnimation")
+    resultText.textContent = "This round is a tie!";
+}
+
+// Event listeners for playing the game
+
 rock.addEventListener('click', function(){
-    playerSelection = "rock";
-    playRound(playerSelection,computerPlay());
-    disable()
+    if (!cooldown) {
+        playerSelection = "rock";
+        playRound(playerSelection,computerPlay());
+        disable()
+    }
 });
 
 paper.addEventListener('click', function(){
-    playerSelection = "paper";
-    playRound(playerSelection,computerPlay());
-    disable();
+    if (!cooldown) {
+        playerSelection = "paper";
+        playRound(playerSelection,computerPlay());
+        disable();
+    }
 });
 
 scissors.addEventListener('click', function(){
-    playerSelection = "scissors";
-    playRound(playerSelection,computerPlay());
-    disable();
+    if (!cooldown) {
+        playerSelection = "scissors";
+        playRound(playerSelection,computerPlay());
+        disable();
+    }
 });
 
+// disable function to prevent spam clicking
 
 function disable(){
-    rock.disabled = true;
-    paper.disabled = true;
-    scissors.disabled = true;
-    setTimeout(function(){rock.disabled = false;},5000);
-    setTimeout(function(){paper.disabled = false;},5000);
-    setTimeout(function(){scissors.disabled = false;},5000);
+    cooldown = true;
+    setTimeout (function(){ cooldown = false}, RECHARGE_TIME);
 }
